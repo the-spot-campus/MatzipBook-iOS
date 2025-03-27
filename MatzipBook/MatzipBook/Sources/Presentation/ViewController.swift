316f55efee990ea6dd560de867c86eee0ec73d8f
RAFA <rafa71802937@gmail.com>
2025-03-27 20:33:35 +0900

    init: 프로젝트 초기 설정

---
+++ MatzipBook/MatzipBook/Sources/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MatzipBook
+//
+//  Created by RAFA on 3/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

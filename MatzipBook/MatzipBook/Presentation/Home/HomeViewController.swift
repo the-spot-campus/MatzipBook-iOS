cda7eff6eaeb658c9fc286c89b2fdc0af43993e9
RAFA <rafa71802937@gmail.com>
2025-05-30 19:31:12 +0900

    add: 각 탭을 구성하는 뷰 및 임시 배경색 지정 코드 추가

---
+++ MatzipBook/MatzipBook/Presentation/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  MatzipBook
+//
+//  Created by RAFA on 5/30/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.mainBackgroundColor
+    }
+}

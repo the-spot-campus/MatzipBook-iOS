720d94aee765e2c3e992931a547d3fa187f209ff
RAFA <rafa71802937@gmail.com>
2025-05-30 16:37:08 +0900

    feat: 아이콘 enum 정의

---
+++ MatzipBook/MatzipBook/Core/DesignSystem/Foundation/MatzipIcon.swift
@@ -0,0 +1,19 @@
+//
+//  MatzipIcon.swift
+//  MatzipBook
+//
+//  Created by RAFA on 5/30/25.
+//
+
+import Foundation
+
+enum MatzipIcon: String {
+    case icBookmarkSelected = "ic_bookmark_selected"
+    case icBookmarkUnselected = "ic_bookmark_unselected"
+    case icHomeSelected = "ic_home_selected"
+    case icHomeUnselected = "ic_home_unselected"
+    case icLocationSelected = "ic_location_selected"
+    case icLocationUnselected = "ic_location_unselected"
+    case icPersonSelected = "ic_person_selected"
+    case icPersonUnselected = "ic_person_unselected"
+}

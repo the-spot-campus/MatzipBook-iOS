28a68e1ff7ec7455eb27d6287a7dc78fae022df0
RAFA <rafa71802937@gmail.com>
2025-05-31 17:29:13 +0900

    refactor: hex 이니셜라이저에서 alpha 파라미터 충돌 해결 및 가독성 개선

--- MatzipBook/MatzipBook/Core/DesignSystem/Foundation/UIColor+.swift
+++ MatzipBook/MatzipBook/Core/DesignSystem/Foundation/UIColor+.swift
@@ -9,7 +9,7 @@ import UIKit
 
 extension UIColor {
 
-    convenience init(hex: String, alpha: CGFloat = 1.0) {
+    convenience init(hex: String, alpha defaultAlpha: CGFloat = 1.0) {
         let scanner: Scanner = Scanner(string: hex)
         _ = scanner.scanString("#")
         var hexNumber: UInt64 = 0
@@ -18,11 +18,11 @@ extension UIColor {
         let red: CGFloat = CGFloat((hexNumber & 0xFF0000) >> 16) / 255.0
         let green: CGFloat = CGFloat((hexNumber & 0x00FF00) >> 8) / 255.0
         let blue: CGFloat = CGFloat(hexNumber & 0x0000FF) / 255.0
-        let alpha: CGFloat = hex.count > 7
+        let finalAlpha: CGFloat = hex.count > 7
         ? CGFloat((hexNumber & 0xFF000000) >> 24) / 255.0
-        : alpha
+        : defaultAlpha
 
-        self.init(red: red, green: green, blue: blue, alpha: alpha)
+        self.init(red: red, green: green, blue: blue, alpha: finalAlpha)
     }
 
     static let mainBackgroundColor: UIColor = UIColor(hex: "FAFAFA")

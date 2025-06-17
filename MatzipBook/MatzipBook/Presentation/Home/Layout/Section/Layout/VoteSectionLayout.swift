d882935a52332a639cb8c09aac3a1f116607ba34
BEOMSU <rafa71802937@gmail.com>
2025-06-17 21:06:36 +0900

    feat: 투표 섹션 전용 Compositional Layout 구성용 VoteSectionLayout 추가

---
+++ MatzipBook/MatzipBook/Presentation/Home/Layout/Section/Layout/VoteSectionLayout.swift
@@ -0,0 +1,34 @@
+//
+//  VoteSectionLayout.swift
+//  MatzipBook
+//
+//  Created by 심범수 on 6/17/25.
+//
+
+import UIKit
+
+final class VoteSectionLayout: SectionLayoutProviding {
+    
+    func layout() -> NSCollectionLayoutSection {
+        let itemSize: NSCollectionLayoutSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(200)
+        )
+        let item: NSCollectionLayoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let group: NSCollectionLayoutGroup = NSCollectionLayoutGroup.horizontal(
+            layoutSize: itemSize,
+            subitems: [item]
+        )
+        
+        let section: NSCollectionLayoutSection = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(
+            top: 25,
+            leading: 20,
+            bottom: 0,
+            trailing: 20
+        )
+        
+        return section
+    }
+}

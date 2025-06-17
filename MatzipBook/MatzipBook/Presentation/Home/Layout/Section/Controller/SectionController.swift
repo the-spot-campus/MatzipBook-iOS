6427e8510ae811492e6035f6c09e38171da7bc6d
BEOMSU <rafa71802937@gmail.com>
2025-06-17 21:08:08 +0900

    feat: UICollectionView 섹션 구성 모듈화를 위한 SectionController 인터페이스 추가

---
+++ MatzipBook/MatzipBook/Presentation/Home/Layout/Section/Controller/SectionController.swift
@@ -0,0 +1,27 @@
+//
+//  SectionController.swift
+//  MatzipBook
+//
+//  Created by 심범수 on 6/17/25.
+//
+
+import UIKit
+
+protocol SectionController {
+    func numberOfItems() -> Int
+    
+    func cellForItem(
+        at indexPath: IndexPath,
+        in collectionView: UICollectionView
+    ) -> UICollectionViewCell
+    
+    func header(
+        in collectionView: UICollectionView,
+        at indexPath: IndexPath
+    ) -> UICollectionReusableView?
+    
+    func footer(
+        in collectionView: UICollectionView,
+        at indexPath: IndexPath
+    ) -> UICollectionReusableView?
+}

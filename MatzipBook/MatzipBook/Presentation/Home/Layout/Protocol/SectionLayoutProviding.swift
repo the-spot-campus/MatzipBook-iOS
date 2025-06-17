3bec860c48c32cd5c81d5b69bce3d3000d5e54f8
BEOMSU <rafa71802937@gmail.com>
2025-06-17 21:01:37 +0900

    feat: 공통 섹션 레이아웃 생성을 위한 SectionLayoutProviding 프로토콜 정의

---
+++ MatzipBook/MatzipBook/Presentation/Home/Layout/Protocol/SectionLayoutProviding.swift
@@ -0,0 +1,12 @@
+//
+//  SectionLayoutProviding.swift
+//  MatzipBook
+//
+//  Created by 심범수 on 6/17/25.
+//
+
+import UIKit
+
+protocol SectionLayoutProviding {
+    func layout() -> NSCollectionLayoutSection
+}

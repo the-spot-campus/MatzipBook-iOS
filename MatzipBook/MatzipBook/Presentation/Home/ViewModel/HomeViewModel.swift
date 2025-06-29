98c7ca257b16588ab5c153ab3c3ce49ae2b46356
BEOMSU <rafa71802937@gmail.com>
2025-06-29 19:59:48 +0900

    refactor: HomeViewModel 프로토콜 제거 및 sections 변수명 개선
    - HomeViewModelProtocol이 불필요한 추상화로 판단되어 제거
    - ViewModel을 교체하거나 다형성을 활용할 요구사항이 없고
      구체 타입으로 충분히 역할을 수행 가능하므로 단순화
    - 코드 가독성과 유지보수성을 위해 인터페이스 중복을 제거

--- MatzipBook/MatzipBook/Presentation/Home/ViewModel/HomeViewModel.swift
+++ MatzipBook/MatzipBook/Presentation/Home/ViewModel/HomeViewModel.swift
@@ -7,21 +7,24 @@
 
 import Foundation
 
-protocol HomeViewModelProtocol {
-    var universityName: String { get }
-    func didTapSearchButton()
-    func didTapSeeAllButton(for section: HomeSection)
-}
-
-final class HomeViewModel: HomeViewModelProtocol {
+final class HomeViewModel {
     
-    var universityName: String { return "부경대학교" }
+    var universityName: String = "부경대학교"
+    private(set) var sections: [SectionDisplayable] = []
+    
+    init() {
+        self.sections = HomeSectionFactory.makeSections(
+            for: universityName
+        ) { [weak self] section in
+            self?.didTapSeeAllButton(for: section)
+        }
+    }
     
     func didTapSearchButton() {
         print("DEBUG: Search button tapped")
     }
     
-    func didTapSeeAllButton(for section: HomeSection) {
-        print("DEBUG: See all button tapped for section >>> \(section.cellIdentifier)")
+    func didTapSeeAllButton(for section: HomeSectionType) {
+        print("DEBUG: See all button tapped for section >>> \(section)")
     }
 }
